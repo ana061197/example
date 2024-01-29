ca1df4591b14e887617c512e796a2ebb0d0f4bf9
ana061197 <158060606+ana061197@users.noreply.github.com>
2024-01-29 16:01:20 +0100

    Add files via upload
    controllers

---
+++ controllers/izvodjaci.js
@@ -0,0 +1,21 @@
+// 3. zadatak
+async function prikaziPocetnuStranicu(req, res, next) {
+    
+}
+
+// 3. zadatak 
+async function unesiRezultateGlasanja(req, res, next) {
+    
+}
+
+
+// 4. zadatak
+async function prikaziIzvestaj(req, res, next) {
+    
+}
+
+module.exports = {
+    prikaziPocetnuStranicu,
+    unesiRezultateGlasanja,
+    prikaziIzvestaj	
+};
